d713da61f9ce7a2a1aad0fa1f037aa7ade22ac13
Luis R. Gonzales <luis@kerb.ai>
2019-02-09 12:57:05 -0800

    Added trackingnet.zip from google drive

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
 wget https://pjreddie.com/media/files/yolov3.weights
+
+cd data/
+wget -O partial-trackingnet.zip 'https://docs.google.com/uc?export=download&id=1xl2GyTxorCcDDfXsf_m2KkZkTv6ZXK9w'
+unzip partial-trackingnet.zip
+rm partial-trackingnet.zip
+cd ..
