a3bac4f46f021a35d01a0a8322f325013c4dc729
Luis R. Gonzales <luis@kerb.ai>
2019-02-22 18:53:56 -0800

    Split init into train and inference

---
+++ init_inference.sh
@@ -0,0 +1,1 @@
+gdown https://drive.google.com/uc?id=1q8TTVFfMl-PLcC-C3i8gIewYmC_tk19c
--- init_train.sh
+++ init_train.sh
@@ -5,5 +5,3 @@ gdown https://drive.google.com/uc?id=1xl2GyTxorCcDDfXsf_m2KkZkTv6ZXK9w
 unzip partial-trackingnet.zip
 rm partial-trackingnet.zip
 cd ..
-
-gdown https://drive.google.com/uc?id=1q8TTVFfMl-PLcC-C3i8gIewYmC_tk19c
